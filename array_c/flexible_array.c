920bcd54f3e683f081d2cacb303cec57b092c6a4
amit9898 <amitchaudhary0539@akgec.ac.in>
2020-03-01 01:41:51 +0530

    This repositoty contains basic examples of c programming for beginners

---
+++ array_c/flexible_array.c
@@ -0,0 +1,20 @@
+//
+
+#include<stdio.h>
+int main()
+{
+    int max,i,mark,tot=0;
+    float avg;
+    printf("Number of students in class: ");
+    scanf("%d",&max);
+    int marks[max];
+    for(i=1;i<=max;i++)
+        {
+        printf("\nMark of std %d: ",i);
+        scanf("%d",&mark);
+        tot+=mark;
+        }
+    avg=(float)tot/(float)max;
+    printf("Average marks of %d students = %.2f\n",max,avg);
+    return 0; 
+}
